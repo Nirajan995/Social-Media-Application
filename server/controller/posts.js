4b05e1783efb2acfccfae0a607874502fe626a4f
Nirajan995 <nirajankunwor604@gmail.com>
2021-10-25 15:50:17 +0545

    mongoose and server setup

---
+++ server/controller/posts.js
@@ -0,0 +1,24 @@
+import PostMessages from "../models/postMessages.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessages.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPosts = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessages(post);
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
