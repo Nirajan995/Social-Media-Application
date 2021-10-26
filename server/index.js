ed83ae8f2d7fd004292a0527288db7edf4e595f9
Nirajan995 <nirajankunwor604@gmail.com>
2021-10-26 10:23:19 +0545

    security leak fixed

--- server/index.js
+++ server/index.js
@@ -3,8 +3,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
 
 app.use("/posts", postRoutes);
 
@@ -12,8 +15,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL =
-  "mongodb+srv://root:root@social-media-app.inij1.mongodb.net/social-media-app?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 const PORT = process.env.PORT || 5000;
 
