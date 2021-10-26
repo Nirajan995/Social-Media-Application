7b377eae8bec096e0c7d7dc295eb9b2e13029004
Nirajan995 <nirajankunwor604@gmail.com>
2021-10-26 08:28:27 +0545

    Frontend

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore, compose } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 import App from "./App";
 
